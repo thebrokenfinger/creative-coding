cc3d60e9a88fda1d78acf19262e510ed30cc4fca
Yatharth K <yatharth01@gmail.com>
2022-06-24 10:51:31 +0530

    arrays used with canvas

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
 let canvas = document.querySelector("canvas");
 let ctx = canvas.getContext("2d");
 
-/* ctx.fillStyle = "papayawhip";
-ctx.fillRect(100, 100, 400, 400); */
-
-// explicit instructions
 ctx.lineWidth = 4;
-ctx.beginPath();
-ctx.rect(100, 100, 400, 400);
-ctx.stroke();
 
-ctx.beginPath();
-ctx.arc(300, 300, 100, 0, 2 * Math.PI);
-ctx.stroke();
+// square dimensions
+const width = 60;
+const height = 60;
+const gap = 20;
+
+function renderCanvasSquares() {
+  for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+      let x = 100 + (width + gap) * i;
+      let y = 100 + (height + gap) * j;
+
+      ctx.beginPath();
+      ctx.rect(x, y, width, height);
+      ctx.stroke();
+
+      // random inner squares
+      if (Math.random() > 0.5) {
+        ctx.beginPath();
+        ctx.rect(x + 8, y + 8, width - 16, height - 16);
+        ctx.stroke();
+      }
+    }
+  }
+}
+
+// uncomment the code below to watch squares rendering at random
+/* setInterval(() => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  renderCanvasSquares();
+}, 600); */
+
+renderCanvasSquares();
